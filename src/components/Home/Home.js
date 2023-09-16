66d6c3c0b9bf73cdbe6fff74c85ddc030cc8815d
MilindiSenevirathne <milindianu@gmail.com>
2023-09-17 02:34:52 +0530

    changed home page

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,6 +1,6 @@
 import React from "react";
 import { Container, Row, Col } from "react-bootstrap";
-import homeLogo from "../../Assets/home-main.svg";
+import homeLogo from "../../Assets/ss4.jpg";
 import Particle from "../Particle";
 import Home2 from "./Home2";
 import Type from "./Type";
@@ -22,7 +22,7 @@ function Home() {
 
               <h1 className="heading-name">
                 I'M
-                <strong className="main-name"> SOUMYAJIT BEHERA</strong>
+                <strong className="main-name"> Milindi Senevirathne</strong>
               </h1>
 
               <div style={{ padding: 50, textAlign: "left" }}>
@@ -30,14 +30,26 @@ function Home() {
               </div>
             </Col>
 
-            <Col md={5} style={{ paddingBottom: 20 }}>
-              <img
-                src={homeLogo}
-                alt="home pic"
-                className="img-fluid"
-                style={{ maxHeight: "450px" }}
-              />
+            <Col md={5} style={{ paddingBottom: "20%" }} className="home-img">
+              <div
+                style={{
+                  width: "80%", 
+                  paddingBottom: "0%", 
+                  borderRadius: "50%",
+                  overflow: "hidden",
+                  marginLeft: "10%"
+                }}
+              >
+                <img
+                  src={homeLogo}
+                  alt="home pic"
+                  className="img-fluid"
+                  style={{ width: "100%", height: "50%", objectFit: "cover", justifyContent:'center' }}
+                />
+              </div>
             </Col>
+
+
           </Row>
         </Container>
       </Container>
---
+++ src/components/Home/Home3.js
@@ -0,0 +1,107 @@
+import React from "react";
+import { Container, Row, Col } from "react-bootstrap";
+import { VerticalTimeline, VerticalTimelineElement } from 'react-vertical-timeline-component';
+import 'react-vertical-timeline-component/style.min.css';
+import MCP from '../../Assets/mcp.png'
+import SS from '../../Assets/ss.png'
+import UOM from '../../Assets/uom.png'
+import AGR from '../../Assets/agr.png'
+import DEV from '../../Assets/dev.png'
+
+function Home3() {
+
+    const style = {
+        contentStyle: {
+            background: "#fff", color: '#142429', borderWidth: 1, borderColor: "#fff", borderRadius: 20
+        },
+        contentArrowStyle: {
+            borderRight: '7px solid  rgb(255, 255, 255)'
+        },
+        iconStyle: {
+            background: 'rgb(13, 26, 29)', display: 'flex', justifyContent: 'center', alignItems: 'center'
+        },
+        h3: {
+            color: "#20c5acd7", fontWeight: 'bolder'
+        },
+        h4: {
+            fontSize: 18, fontWeight: 'bold'
+        }
+    }
+
+    const details = [
+        {
+            id: 1,
+            title: 'G.C.E. O/L',
+            subtitle: 'Mahanama College, Panadura - 2015',
+            details: '8 A"s, 1 B',
+            icon: MCP
+        },
+        {
+            id: 2,
+            title: 'G.C.E. A/L',
+            subtitle: 'Sumangala Balika Maha Vidyalaya - 2018',
+            details: '2 A"s, 1 C - Bio Stream',
+            details2: 'Z-Score - 1.6534',
+            icon: SS
+        },
+        {
+            id: 3,
+            title: 'Bsc. (Hons) in Information Technology',
+            subtitle: 'University of Moratuwa (2020 - 2024)',
+            details: 'CGPA - 3.67',
+            icon: UOM
+        },
+        {
+            id: 4,
+            title: 'Software Engineer Intern',
+            subtitle: 'Agrithmics (Pvt) Ltd (Dec 2022 - June 2023)',
+            details: 'React Native, Android',
+            icon: AGR
+        },
+        {
+            id: 5,
+            title: 'Technical Lead',
+            subtitle: 'DevLabs (From August 2023)',
+            details: 'UI/UX',
+            icon: DEV
+        }
+    ]
+
+    return (
+        <Container fluid className="home-about-section" id="about">
+            <Container>
+                <h1 style={{ fontSize: "2.6em" }} className="home-about-description">
+                    My <span className="purple"> Journey </span>
+                </h1>
+                <VerticalTimeline>
+                    {details?.map((item) => {
+                        return (
+                            <VerticalTimelineElement
+                                className="vertical-timeline-element--work"
+                                contentStyle={style.contentStyle}
+                                contentArrowStyle={style.contentArrowStyle}
+                                date="2011 - present"
+                                iconStyle={style.iconStyle}
+                                icon={<img src={item.icon} alt="Custom Icon" style={{ width: '80%', height: 'auto' }} />}
+                            >
+                                <div style={{ textAlign: 'left' }}>
+                                    <h3 style={style.h3}>{item.title}</h3>
+                                    <h4 style={style.h4}>{item.subtitle}</h4>
+                                    <p>
+                                        {item.details}
+                                    </p>
+                                    {item.details2 && (
+                                        <p>
+                                            {item.details2}
+                                        </p>
+                                    )}
+                                </div>
+                            </VerticalTimelineElement>
+                        )
+                    })}
+                </VerticalTimeline>
+            </Container>
+        </Container>
+    );
+}
+export default Home3;
